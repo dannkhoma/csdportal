8394e0092be22a0162f55c4baab486fb0e1f9aa9
Charles Chigoriwa <cchigoriwa@gmail.com>
2015-10-30 20:44:34 +0200

    Search Results now coming from OpenInfoman

--- src/main/java/zw/hitrac/csdportal/controller/Home.java
+++ src/main/java/zw/hitrac/csdportal/controller/Home.java
@@ -1,10 +1,13 @@
 package zw.hitrac.csdportal.controller;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import zw.co.hitrac.zhris.csd.adapter.common.domain.WrappedEntity;
+import zw.co.hitrac.zhris.csd.adapter.common.util.EntityFinder;
 
 /**
  *
@@ -18,11 +21,14 @@ public class Home {
         return "index";
     }
 
-    @RequestMapping(value="/search/result",method = RequestMethod.GET)
+    @RequestMapping(value = "/search/result", method = RequestMethod.GET)
     public String searchResult(@RequestParam(name = "q", required = false) String q, Model model) {
-        if (q == null || q.trim().length()<3) {
+        if (q == null || q.trim().length() < 3) {
             return "index";
         } else {
+            String address = "http://192.168.1.22:8984/CSD/csr/mohcc/careServicesRequest";
+            List<WrappedEntity> wrappedEntities = EntityFinder.getEntitiesFromAllDirectories(q, address);
+            model.addAttribute("wrappedEntities", wrappedEntities);
             model.addAttribute("q", q);
             return "searchResults";
         }
